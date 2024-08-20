4c2e6e42458855f8145bddb8613655c374994294
Nani2512 <badavathnaresh3@gmail.com>
2024-08-20 13:35:03 +0530

    updated new files

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+  description = "ami id "
+  type = string
+  default = "ami-066784287e358dad1"
+}
+variable "type" {
+  description = "instance type"
+  type = string
+  default = "t2.micro"
+}
+variable "key" {
+  description = "keypair name"
+  type = string
+  default = "mykeypair"
+}
+variable "region" {
+  description = "region name us"
+  type = string
+  default = "us-east-1"
+}
