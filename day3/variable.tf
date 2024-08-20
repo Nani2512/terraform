4d0994b1b66eaa7bacd19294c02c9d540097f277
Nani2512 <badavathnaresh3@gmail.com>
2024-08-20 14:54:28 +0530

    new changes have done in day3

---
+++ day3/variable.tf
@@ -0,0 +1,11 @@
+variable "bucketname" {
+    description = "s3 bucket value"
+    type = string
+    default = "nareshnanibucket"
+  
+}
+variable "region" {
+  type = string
+    default = "us-west-2"
+  
+}
