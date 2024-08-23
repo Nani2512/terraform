a13424d62d2c2980878051ca1ee6b4fd4821a23c
Nani2512 <badavathnaresh3@gmail.com>
2024-08-23 13:38:23 +0530

    day4 task updated

---
+++ day4/variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "ami vlaue"
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+  description = "value"
+  type = string
+  default = ""
+
+}
+variable "key_name" {
+  description = ""
+  type = string
+  default = ""
+}
+variable "region" {
+  default = ""
+}
