13c406a87fda3e1131375e38dc4a135092d075f2
Nani2512 <badavathnaresh3@gmail.com>
2024-08-27 15:29:02 +0530

    created ALB and TG

--- day6/main.tf
+++ day6/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
     ami="ami-066784287e358dad1"
-instance_type = "t2.micro"
+instance_type = "t2.nano"
 availability_zone = "us-east-1a"
 key_name = "mykeypair"
 tags = {
